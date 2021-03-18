11c5f0d219349bfe64b96c2cc8b517743f7097be
Robert Laverty <roblav96@gmail.com>
2021-03-18 00:36:05 -0400

    [Darwin] M  scripts/rarbg.to/DebridsCached.ts

--- scripts/rarbg.to/DebridsCached.ts
+++ scripts/rarbg.to/DebridsCached.ts
@@ -7,7 +7,11 @@
 // @noframes
 // ==/UserScript==
 
+// import { StandardWebSocketClient } from 'https://deno.land/x/websocket/mod.ts'
+
 document.addEventListener('readystatechange', (event) => {
 	if (document.readyState != 'complete') return
-	console.warn('document.readyState ->', document.readyState)
+	console.error('document.readyState ->', document.readyState)
+	// let ws = new StandardWebSocketClient('ws://127.0.0.1:13874')
+	// console.log('ws ->', ws)
 })
--- src/bundler.ts
+++ src/bundler.ts
@@ -3,42 +3,51 @@ import * as path from 'https://deno.land/std/path/mod.ts'
 import { DIRS } from './mod.ts'
 
 export async function bundle(fpath: string) {
-	console.info('bundle ->', fpath)
+	// console.info('bundle ->', fpath)
 	let file = await Deno.open(fpath, { read: true })
 	let fstat = await Deno.fstat(file.rid)
 	if (!fstat.isFile) return
 
 	let distpath = path.join(DIRS.dist, path.relative(DIRS.scripts, fpath))
 	distpath = distpath.replace(/\.ts$/, '.user.js')
-	let disturl = path.toFileUrl(distpath).toString()
 
 	let fsource = await Deno.readTextFile(fpath)
 	fsource = fsource.replace(
 		'// ==/UserScript==',
-		`// @downloadURL ${disturl}\n// ==/UserScript==`,
+		`// @downloadURL ${path.toFileUrl(distpath).toString()}\n// ==/UserScript==`,
 	)
 	fsource = fsource.replace(
 		'// ==/UserScript==',
 		`// @version ${fstat.mtime?.valueOf()}\n// ==/UserScript==`,
 	)
+	let header = fsource.split('// ==UserScript==')[1].split('// ==/UserScript==')[0]
+	header = `// ==UserScript==${header}// ==/UserScript==`
 
-	let { files } = await Deno.emit(fpath, {
-		sources: { [fpath]: fsource },
-		// check: false,
-		// bundle: 'esm',
-		compilerOptions: {
-			// allowUnreachableCode: true,
-			// inlineSourceMap: true,
-			// inlineSources: true,
-			lib: ['dom', 'esnext'],
-			removeComments: false,
-			sourceMap: false,
-		},
+	let bundle = Deno.run({
+		cmd: ['deno', 'bundle', '--unstable', '--no-check', fpath, distpath],
 	})
-	for (let [outurl, outsource] of Object.entries(files)) {
-		outurl = outurl.replace(/\.ts\.js$/, '.user.js')
-		let outpath = path.join(DIRS.dist, path.relative(DIRS.scripts, path.fromFileUrl(outurl)))
-		await fs.ensureDir(path.dirname(outpath))
-		await Deno.writeTextFile(outpath, outsource)
-	}
+	await bundle.status()
+	bundle.close()
+	await Deno.writeTextFile(distpath, `${header}\n\n${await Deno.readTextFile(distpath)}`)
+
+	// let { files } = await Deno.emit(fpath, {
+	// 	// sources: { [fpath]: fsource },
+	// 	check: false,
+	// 	bundle: 'esm',
+	// 	compilerOptions: {
+	// 		// allowUnreachableCode: true,
+	// 		// inlineSourceMap: true,
+	// 		// inlineSources: true,
+	// 		// lib: ['dom', 'esnext'],
+	// 		// removeComments: false,
+	// 		// sourceMap: true,
+	// 	},
+	// })
+	// for (let [outurl, outsource] of Object.entries(files)) {
+	// 	// outurl = outurl.replace(/\.ts\.js$/, '.user.js')
+	// 	// let outpath = path.join(DIRS.dist, path.relative(DIRS.scripts, path.fromFileUrl(outurl)))
+	// 	await fs.ensureDir(path.dirname(distpath))
+	// 	await Deno.writeTextFile(distpath, `${header}\n\n${outsource}`)
+	// }
+
 }
