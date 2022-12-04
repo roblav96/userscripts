35578928c45c4bb0ada108460329fb831cf179a6
Robert Laverty <roblav96@gmx.com>
2022-12-04 04:10:26 -0500

    trimmer

--- scripts/amazon.com/TabTitleTrim.ts
+++ scripts/amazon.com/TabTitleTrim.ts
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name TabTitleTrim@amazon.com
-// @match https://*.amazon.com/*
+// @name TabTitleTrim@www.amazon.com
+// @match https://www.amazon.com/*
 // @noframes
 // ==/UserScript==
 
 import type {} from '../../types/violentmonkey.d.ts'
 
-function TabTitleTrim(event: Event) {
-	console.log('readystatechange ->', `[${document.readyState}]`, event)
-	let replacer = /^amazon\.com ?: /i
-	if (replacer.test(document.title)) {
-		document.title = document.title.replace(replacer, '').trim()
-	}
+// function TabTitleTrim(event: Event) {
+console.log('readystatechange ->', `[${document.readyState}]`, event)
+let replacer = /^amazon\.com ?: /i
+if (replacer.test(document.title)) {
+	document.title = document.title.replace(replacer, '').trim()
 }
-document.addEventListener('readystatechange', TabTitleTrim, { once: true })
+// }
+// document.addEventListener('readystatechange', TabTitleTrim, { once: true })
