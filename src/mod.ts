34365be9907f8585a18130b356e74077403ee8b1
Robert Laverty <roblav96@gmx.com>
2023-01-08 02:00:41 -0500

    better mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -6,29 +6,30 @@ import * as http from 'https://deno.land/std/http/mod.ts'
 import * as path from 'https://deno.land/std/path/mod.ts'
 import bundler from './bundler.ts'
 
-export const DIRS = new (class {
-	root = path.dirname(path.dirname(path.fromFileUrl(import.meta.url)))
-	scripts = path.join(this.root, 'scripts')
-	dist = path.join(this.root, 'dist')
-})()
+export const ROOT = path.dirname(path.dirname(path.fromFileUrl(import.meta.url)))
+export const SCRIPTS = path.join(ROOT, 'scripts')
+export const DIST = path.join(ROOT, 'dist')
 
-if (await fs.exists(DIRS.dist)) {
-	await Deno.remove(DIRS.dist, { recursive: true })
+if (await fs.exists(DIST)) {
+	await Deno.remove(DIST, { recursive: true })
 }
 
-for await (let entry of fs.walk(DIRS.scripts, { exts: ['.ts'], skip: [/\.d\.ts$/] })) {
+for await (let entry of fs.walk(SCRIPTS, {
+	exts: ['.ts'],
+	includeDirs: false,
+	skip: [/\.d\.ts$/],
+})) {
 	await bundler(entry.path)
 }
 
-const SRCPATHS = new Set<string>()
+const srcpaths = new Set<string>()
 const debounce = async.debounce(async () => {
-	let srcpaths = Array.from(SRCPATHS)
-	SRCPATHS.clear()
 	for (let srcpath of srcpaths) {
 		await bundler(srcpath)
 	}
+	srcpaths.clear()
 }, 100)
-for await (let { paths } of Deno.watchFs([DIRS.scripts])) {
-	paths.forEach((v) => SRCPATHS.add(v))
+for await (let event of Deno.watchFs(SCRIPTS)) {
+	event.paths.forEach((v) => srcpaths.add(v))
 	debounce()
 }
